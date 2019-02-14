18597fa5b82aa80637ed08e38aab21d30a575f81
Steven Kulp <steven@MOCHA-CHAI.localdomain>
2019-02-14 14:40:30 -0800

    added some linking

--- app/javascript/pages/AllListings.js
+++ app/javascript/pages/AllListings.js
@@ -1,5 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
 
 import Cards from '../components/Cards'
 
@@ -20,6 +26,9 @@ class AllListings extends React.Component {
     return (
       <div className='listings__container'>
         <h1>All Listings</h1>
+        <a className="waves-effect waves-light btn add" href='/add_tools'>
+          <i className="material-icons left">add</i>Create New Listing
+        </a>
       <div class="grid__listing">
         {this.state.tools.map((tool, index) =>
           <Cards key={index}
