d3ceb0b4b11cc4494dd61a76d5449ba73ce18783
bcree11 <b_cree@yahoo.com>
2019-02-14 09:33:48 -0800

    added devise

--- app/javascript/components/pages/MyTools.js
+++ app/javascript/components/pages/MyTools.js
@@ -8,45 +8,45 @@ import {
 } from 'react-router-dom'
 
 const MyTools = (props) => {
-    // debugger
-    return (
-      <div>
-        <h1>My Tools</h1>
-        <table className='striped responsive-table' style={{width:'60vw'}}>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Model</th>
-              <th>Serial Number</th>
-              <th>Price Per Day</th>
-              <th>Price Per Hour</th>
-              <th>Zip Code</th>
-            </tr>
-            </thead>
-            <tbody>
-            {props.tools.map((tool, index) =>
-            <tr key={index}>
-              <td>{tool.title}</td>
-              <td>{tool.model}</td>
-              <td>{tool.serialnumber}</td>
-              <td>{tool.price}</td>
-              <td>Need to add</td>
-              <td>{tool.zipcode}</td>
-              <td>{tool.id}</td>
-              <td>
-                <a id='deleteButton' className="waves-effect waves-light btn-small" type='submit' onClick={() => props.handleDelete(tool.id)} rel="nofollow">
-                  <i className="material-icons left">remove</i>Delete
-                </a>
-              </td>
-            </tr>
-          )}
-          </tbody>
-        </table>
-        <a className="waves-effect waves-light btn add" href='/add_tools'>
-          <i className="material-icons left">add</i>Add Tools
-        </a>
-      </div>
-    );
+  return (
+    <div>
+      <h1>My Tools</h1>
+      <table className='striped responsive-table' style={{width:'60vw'}}>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Model</th>
+            <th>Serial Number</th>
+            <th>Price Per Day</th>
+            <th>Price Per Hour</th>
+            <th>Zip Code</th>
+          </tr>
+          </thead>
+          <tbody>
+          {props.tools.map((tool, index) =>
+          <tr key={index}>
+            <td>{tool.title}</td>
+            <td>{tool.model}</td>
+            <td>{tool.serialnumber}</td>
+            <td>{tool.price}</td>
+            <td>Need to add</td>
+            <td>{tool.zipcode}</td>
+            <td>{tool.id}</td>
+            <td>{tool.user_id}</td>
+            <td>
+              <a id='deleteButton' className="waves-effect waves-light btn-small" type='submit' onClick={() => props.handleDelete(tool.id)} rel="nofollow">
+                <i className="material-icons left">remove</i>Delete
+              </a>
+            </td>
+          </tr>
+        )}
+        </tbody>
+      </table>
+      <a className="waves-effect waves-light btn add" href='/add_tools'>
+        <i className="material-icons left">add</i>Add Tools
+      </a>
+    </div>
+  );
 }
 
 export default MyTools
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_040705) do
+ActiveRecord::Schema.define(version: 2019_02_14_043943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 2019_02_12_040705) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
+    t.string "displayname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
