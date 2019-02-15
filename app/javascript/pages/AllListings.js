bc2891d782026c72b22415c64a7546e8cdafb628
bcree11 <b_cree@yahoo.com>
2019-02-15 11:33:48 -0800

    adjustments

--- app/javascript/pages/AllListings.js
+++ app/javascript/pages/AllListings.js
@@ -27,9 +27,9 @@ class AllListings extends React.Component {
       <div className='listings__container'>
         <h1>All Listings</h1>
 
-        
+
         <a className="waves-effect waves-light btn add" href='/add_tools'>
-        <i className="material-icons left">add</i>Create New Listing
+          <i className="material-icons left">add</i>Create New Listing
         </a>
       <div className="grid__listing">
 
