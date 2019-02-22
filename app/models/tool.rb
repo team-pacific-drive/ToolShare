12ab2ea18e99febf2e4ab3aedfdf44aeb435995d
Steven Kulp <steven@MOCHA-CHAI.localdomain>
2019-02-22 09:29:33 -0800

    updates to master

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -3,10 +3,6 @@ class Tool < ApplicationRecord
   delegate :firstname, :lastname, to: :user, prefix: true
 
   validates :title, :model, :description, :price, presence: true
-<<<<<<< HEAD
-
-=======
->>>>>>> 9a062f426d570ac699e6690bb99de6f105360cd3
   validates :description, length: { maximum: 144 }
 
 end
