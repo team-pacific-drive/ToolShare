504e879a2f233a33d27f031ea40f93f6369a4079
Fresh-mex <justingarcia@DESKTOP-K11FE8L.localdomain>
2019-02-22 13:23:55 -0800

    update master

--- app/javascript/pages/AboutPage.js
+++ app/javascript/pages/AboutPage.js
@@ -15,7 +15,7 @@ class AboutPage extends React.Component{
         </div>
         <div className="row">
           <p className="column-text-logo">Taking on another home repair project? Or pheraps you need that one time use of a tool that is $200 down at the local hardware store. No need to worry. ToolShare will save you the time and the money. ToolShare was created to allow users to list and rent tools right out of there local community. This application can also be very useful for those homeowners with a mess of tools stacked up their garage that just never get any use. Knock the dust off of your tools and get some use and money out those paper weights. At the same time help out some local community members in need of some serious assisstance. This is a one of a kind application that is very simple to use. Simply sign up and begin the process. The possibilities are endless. Help others in your community help you! </p>
-          <img src={tool3} className="column-text-logo" alt="" />
+          <img src={tool3} className="column-text-logo" alt="tool-image" />
         </div>
         <h1>Meet the team!</h1>
         <div className="row">
