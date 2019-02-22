8a53c3f619c73ab020eeafba891b24871d3c2a93
Eric Ream <Ream@Jordans-Air.localdomain>
2019-02-22 11:35:52 -0800

    updated sign out route and back to my listings authenticated route

--- app/javascript/components/NavBar.js
+++ app/javascript/components/NavBar.js
@@ -31,7 +31,8 @@ const NavBar = (props) => {
             <a href="/users/sign_in"><li className="navi-element login-register"><b>Sign In / Join</b></li></a>
             }
             {props.firstname  &&
-            <a href="/users/sign_out"><li className="navi-element sign-out">Sign Out</li></a>
+            <a href="/users/sign_out" rel="nofollow" data-method="delete"><li className="navi-element sign-out"><b>Sign Out</b></li></a>
+            // <a rel="nofollow" data-method="delete" href="/users/sign_out">Sign Out</a>
             }
           </ul>
         </div>
