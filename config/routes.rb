b5fe53ef46e1cca297e8fce88ba5088e92966a4e
bcree11 <b_cree@yahoo.com>
2019-02-25 14:39:05 -0800

    added user chat and momentjs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :tools
+
   devise_for :users
+
+  resources :conversations do
+    resources :messages
+  end
+
   get '/*path', to: 'pages#index', constraints: ->(request){ request.format.html? }
   root to: 'pages#index'
 end
