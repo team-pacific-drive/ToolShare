e30289cd16390469fa94e4502384d1cb118fc233
Fresh-mex <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-02-26 10:44:30 -0800

    meger with the master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-<<<<<<< HEAD
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :cross_street, :city, :state, :zip_code, :phone_number)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:firstname, :lastname, :email, :cross_street, :city, :state, :zip_code, :phone_number, :password, :current_password)}
-=======
 
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :cross_street, :city, :state, :zip_code, :phone_number)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:firstname, :lastname, :email, :cross_street, :city, :state, :zip_code, :phone_number, :password, :current_password)}
 
->>>>>>> master
   end
 end
