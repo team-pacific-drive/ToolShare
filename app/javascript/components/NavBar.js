95aafb46260d7f4a5c0a36abc9a009d38f56da94
Steven Kulp <steven@MOCHA-CHAI.localdomain>
2019-02-28 15:02:29 -0800

    Minor tweaks

--- app/javascript/components/NavBar.js
+++ app/javascript/components/NavBar.js
@@ -15,12 +15,21 @@ class NavBar extends React.Component{
     }
   }
 
+  toggleMenu() {
+    var x = document.getElementById("nav-menu");
+    if (x.className === "navi-items") {
+      x.className += " mobile-menu";
+    } else {
+      x.className = "navi-items";
+    }
+  }
+
   render(){
     let { current_user } = this.props
     return(
       <div className="navi-span">
         <div className="navi-container">
-          <div className="toolshare">
+          <div className="logo-container">
             <a href="/">
               <div className="logo">
                 <img src={Logo} height="32" width="32" />
@@ -28,22 +37,30 @@ class NavBar extends React.Component{
               </div>
             </a>
           </div>
-          <ul className="navi-items">
-            <a href="/"><li className="navi-element"><b>Rent a Tool</b></li></a>
+          <ul className="navi-items" id="nav-menu">
             {current_user &&
               <React.Fragment>
-                <a href="/account/add_tools"><li className="navi-element"><b>Add a Tool</b></li></a>
                 <a href="/about"><li className="navi-element"><b>About</b></li></a>
+                <a href="/"><li className="navi-element"><b>Listings</b></li></a>
+                <a href="/account/add_tools"><li className="navi-element"><b>Add a Tool</b></li></a>
                 <a href="/account/my_tools"><li className="navi-element"><b>{this.addingToEndofName()} Tools</b></li></a>
+                <a href="/conversations"><li className="navi-element"><b>Messages</b></li></a>
                 <a href="/users/sign_out" rel="nofollow" data-method="delete"><li className="navi-element sign-out"><b>Sign Out</b></li></a>
+                <a href="javascript:void(0);" onClick="toggleMenu()">
+                  <h3 className="mobile-menu">Menu</h3>
+                </a>
               </React.Fragment>
             }
             {!current_user &&
               <React.Fragment>
-                <a href="/users/sign_in"><li className="navi-element"><b>Add a Tool</b></li></a>
                 <a href="/about"><li className="navi-element"><b>About</b></li></a>
+                <a href="/"><li className="navi-element"><b>Listings</b></li></a>
+                <a href="/users/sign_in"><li className="navi-element"><b>Add a Tool</b></li></a>
                 <a href="/users/sign_in"><li className="navi-element"><b>Sign In</b></li></a>
                 <a href="/users/sign_up"><li className="navi-element login-register"><b>Join</b></li></a>
+                <a href="javascript:void(0);" onClick="toggleMenu()">
+                  <h3 className="mobile-menu">Menu</h3>
+                </a>
               </React.Fragment>
             }
           </ul>
